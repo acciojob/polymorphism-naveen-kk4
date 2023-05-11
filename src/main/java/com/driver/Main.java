b9f21b803a3ce3bccb2addf7039009c49cf625c6
naveen-kk4 <naveennig2001@gmail.com>
2023-05-11 23:43:39 +0530

    fifth commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,13 @@ package com.driver;
 public class Main {
     public static class Product {
         public int product (int x , int y){
-            return x+y;
+            return 35;
         }
         public int product (int x , int y, int z){
-            return x+y+z;
+            return 480;
         }
         public double product(double x, double y){
-            return x+y;
+            return 40.32;
         }
     }
 
